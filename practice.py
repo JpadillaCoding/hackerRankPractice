9ef5d426f36551bc65690c06fb139fba5f6cf42b
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-21 00:57:03 -0600

    subarray division: solved

--- practice.py
+++ practice.py
@@ -1,16 +1,14 @@
 def birthday(s, d, m):
-    # loop through S to find sums of D with the length of M
-    # outer loop to go through full array
     counter = 0
     arrLen = len(s)
-    # edge case of length of array is greater than M
+    if arrLen < m:
+        return 0
     for i in range(arrLen):
-        # inner loop only iterates the length of M  
-        print(s[i:i+m])
-            # check if the sum of those are equal to D 
-                # if so add to counter
-                # edge case of making single it is a length of m 
-    # return counter 
+        subArray = (s[i:i+m])
+        if len(subArray) == m:
+            if sum(subArray) == d:
+                counter += 1
+    return (counter)
 
 
 birthday([2,2,1,3,2], 4, 2)
