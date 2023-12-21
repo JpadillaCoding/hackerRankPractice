8f7fc31ac279b808f66326caa4959d3fb9560f7d
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-21 00:52:04 -0600

    subarray division: added edge cases

--- practice.py
+++ practice.py
@@ -1,9 +1,15 @@
 def birthday(s, d, m):
     # loop through S to find sums of D with the length of M
     # outer loop to go through full array
-        # inner loop only iterates the length of M 
+    counter = 0
+    arrLen = len(s)
+    # edge case of length of array is greater than M
+    for i in range(arrLen):
+        # inner loop only iterates the length of M  
+        print(s[i:i+m])
             # check if the sum of those are equal to D 
                 # if so add to counter
+                # edge case of making single it is a length of m 
     # return counter 
 
 
