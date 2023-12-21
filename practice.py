9669587847a1e9c0d1da909082ef137ffcecf029
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-20 23:37:18 -0600

    counting sort 1

--- practice.py
+++ practice.py
@@ -1,21 +1,7 @@
-def plusMinus(arr):
+def countingSort(arr):
     # Write your code here
-    n = len(arr)
-    positives = 0
-    negatives = 0
-    zeros = 0
-    for i in range(n):
-        if (arr[i] > 0):
-            positives += 1
-        elif (arr[i] == 0):
-            zeros += 1
-        else:
-            negatives += 1
-            
-    positiveRatio = positives / n
-    negativeRatio = negatives / n
-    zeroRatio = zeros / n
-
-    print(f"{positiveRatio:.6f}")
-    print(f"{negativeRatio:.6f}")
-    print(f"{zeroRatio:.6f}")
+    arrayLength = len(arr)
+    sortArray = [0] * 100
+    for i in range(arrayLength):
+        sortArray[arr[i]] += 1
+    return sortArray
