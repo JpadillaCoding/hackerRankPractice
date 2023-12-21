926a61f3ea66f4eafb1fa992656a9761da9f305c
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-20 23:36:16 -0600

    plus minus

--- practice.py
+++ practice.py
@@ -1,7 +1,21 @@
-def miniMaxSum(arr):
+def plusMinus(arr):
     # Write your code here
-    arrLength = len(arr)
-    arr.sort()
-    minTotal = sum(arr[0:arrLength-1])
-    maxTotal = sum(arr[1:arrLength])
-    print(minTotal, maxTotal)
+    n = len(arr)
+    positives = 0
+    negatives = 0
+    zeros = 0
+    for i in range(n):
+        if (arr[i] > 0):
+            positives += 1
+        elif (arr[i] == 0):
+            zeros += 1
+        else:
+            negatives += 1
+            
+    positiveRatio = positives / n
+    negativeRatio = negatives / n
+    zeroRatio = zeros / n
+
+    print(f"{positiveRatio:.6f}")
+    print(f"{negativeRatio:.6f}")
+    print(f"{zeroRatio:.6f}")
