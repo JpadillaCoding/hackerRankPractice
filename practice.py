7d5192f707787b769ca7c657c426fc2284a4ea09
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-20 23:38:40 -0600

    time conversion

--- practice.py
+++ practice.py
@@ -1,10 +1,22 @@
-def lonelyinteger(a):
+def timeConversion(s):
     # Write your code here
-    myList = []
-    argLen = len(a)
-    for x in range(argLen):
-        if a[x] not in myList:
-            myList.append(a[x])
-        else:
-            myList.remove(a[x])
-    return myList[0]
+    # if time is < 12, return in same time but without am/pm 
+    # if >12, do 12+ the time
+    # with the answer remove am/pm 
+    if 'AM' in s:
+        s = s[:8]
+        # get without am
+        hour = int(s[:2])
+        # get the hour
+        if (hour == 12):
+            s = s.replace(s[:2], "00")
+            return s
+        return s
+    else:
+        s = s[:8]
+        hour = int(s[:2])
+        if hour == 12:
+            return s
+        millitaryHour = str(hour + 12)
+        s = s.replace(s[0:2], millitaryHour)
+        return s
