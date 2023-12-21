dece5df97ebd54f4b16bb465867bb7ac84bdb382
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-20 23:48:20 -0600

    solution: initial solution

--- practice.py
+++ practice.py
@@ -1,8 +1,14 @@
+from string import ascii_lowercase
 def pangrams(s):
     # to lower the string
+    s.lower()
     # loop through alphabet ascii
+    for letter in ascii_lowercase:
     # check if the letter is in string
+        if letter not in s:
     # if not return "not pangram"
+            return ("not pangram")
     # end of loop reuturn "pangram"
+    return ("pangram")
 
-pangrams("The quick brown fox jumps over the lazy dog")
+pangrams("The quick brown fox jumps over the lazy do")
