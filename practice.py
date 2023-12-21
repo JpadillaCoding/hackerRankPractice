08e6440671a1467e481be475d20efb3c3042014a
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-20 23:44:42 -0600

    solving: psuedoCode

--- practice.py
+++ practice.py
@@ -1,22 +1,8 @@
-def timeConversion(s):
-    # Write your code here
-    # if time is < 12, return in same time but without am/pm 
-    # if >12, do 12+ the time
-    # with the answer remove am/pm 
-    if 'AM' in s:
-        s = s[:8]
-        # get without am
-        hour = int(s[:2])
-        # get the hour
-        if (hour == 12):
-            s = s.replace(s[:2], "00")
-            return s
-        return s
-    else:
-        s = s[:8]
-        hour = int(s[:2])
-        if hour == 12:
-            return s
-        millitaryHour = str(hour + 12)
-        s = s.replace(s[0:2], millitaryHour)
-        return s
+def pangrams(s):
+    # to lower the string
+    # loop through alphabet ascii
+    # check if the letter is in string
+    # if not return "not pangram"
+    # end of loop reuturn "pangram"
+
+pangrams("The quick brown fox jumps over the lazy dog")
