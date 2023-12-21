d72560231af628de33aaafcb6627a8ea04961c3e
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-20 23:35:24 -0600

    mini-max sum

--- practice.py
+++ practice.py
@@ -1,16 +1,7 @@
-def flippingBits(n):
-    bitFormat = list('{:032b}'.format(n))
-    bitFormatLen = len(bitFormat)
-# loop through each bit and flip
-    for bit in range(bitFormatLen):
-        if bitFormat[bit] == '0':
-            bitFormat[bit] = '1'
-        elif bitFormat[bit] == '1':
-            bitFormat[bit] = '0'
-# covert back to int
-    bitFormat = ''.join(bitFormat)
-    n = int(bitFormat,2)
-# return opposite of bit formate fort each one
-    return(n)
-
-flippingBits(2147483647) 
+def miniMaxSum(arr):
+    # Write your code here
+    arrLength = len(arr)
+    arr.sort()
+    minTotal = sum(arr[0:arrLength-1])
+    maxTotal = sum(arr[1:arrLength])
+    print(minTotal, maxTotal)
