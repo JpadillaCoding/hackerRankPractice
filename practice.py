1bda8d09f871d142b13efd9e0167e41456eb923b
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-21 23:56:23 -0600

    409 longest palindrome: partial solve

--- practice.py
+++ practice.py
@@ -2,19 +2,23 @@ def longestPalindrome(s):
     # loop through and make a hashmap of all the items
     items = {}
     evenCount = 0
+    if len(s) == 1 or len(s) == 2:
+        return 1
+
     for i in range(len(s)):
         items[s[i]] = 1 + items.get(s[i],0)
         # if item i not contained then init it
         if items[s[i]] % 2 == 0:
             evenCount +=1
     # check hashmap for number of even numbers
+    evenCount = evenCount * 2
     if evenCount > 0:
         if evenCount < len(s):
-            print(evenCount + 1)
+            return (evenCount + 1)
         else: 
-            print(evenCount)
+            return (evenCount)
     else: 
-        print(0)
+        return (0)
         
     # if the amount of items in hashmap is even retunr than number, if odd then return that number
 longestPalindrome("abccccdd")
