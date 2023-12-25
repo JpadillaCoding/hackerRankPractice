b7fbc89b74640a43e940b284361d907e4574602e
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-25 12:27:01 -0600

    2379: min recolord for consecutive blocks: solved

--- practice.py
+++ practice.py
@@ -1,28 +1,20 @@
-def minimumDifference(nums,k):
+def minimumRecolors(blocks,k):
+    #check if k is in block by multiplying the k by b 
+    # if not then loop 
+    # make a window of 7 looking for the container with the most amount of black
+    # loop in window to count b
+    # return the difference between highest and  k 
+    quickCheckSet = ''.join(['B']*k)
+    highestBlack = 0
+    if quickCheckSet in blocks:
+        print(0)
+    else: 
+        for i in range(len(blocks)-k+1):
+            window = blocks[i: i+k]
+            totalBlacks = 0
+            for char in range(len(window)):
+                if window[char] == 'B': totalBlacks += 1
+            if totalBlacks > highestBlack: highestBlack = totalBlacks
 
-    # do i need to sort? 
-    # sort the array
-    # make a variable to track the current lowest diff
-    # loop through all and comparing differences between [0]and[-1]
-    # sliding window for comparison 
-    # use k to get the window
-    # return lowest diff variable
-
-    nums.sort()
-    lowestDelta = 0
-    if len(nums) == 1 :
-        lowestDelta = 0
-    elif len(nums) == k:
-        lowestDelta = nums[-1] - nums[0]
-    else:
-        lowestDelta = nums[k] - nums[0]
-    for i in range(len(nums)-k+1):
-        windowDelta = nums[i:i+k]
-        highest = windowDelta[-1]
-        lowest = windowDelta[0]
-        delta = highest - lowest
-
-        if (lowestDelta > delta):
-            lowestDelta = delta
-    print(lowestDelta)
-minimumDifference([4407,3036], 2)
+    print(k-highestBlack)
+minimumRecolors("WBWBBBW",2)
