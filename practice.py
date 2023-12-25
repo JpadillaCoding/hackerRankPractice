40db2e67a3dd4eccc695dbe6a94fd285ba68dfcd
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-25 12:01:26 -0600

    1984 min delta highest lowest: solved

--- practice.py
+++ practice.py
@@ -1,16 +1,28 @@
-def divisorSubstrings(num,k):
+def minimumDifference(nums,k):
 
-    # make string into array 
-    stringNum = str(num)
-    splitNum = []
-    count = 0
-    for i in range(len(stringNum)): splitNum.append(stringNum[i])
-    # use k to get the "window" needed to 
-    for i in range(0,len(splitNum)-k+1):
-        currentNum = splitNum[slice(i,i+k)]
-        joinedNum = int(''.join(currentNum))
-        if joinedNum != 0 :
-            if num % joinedNum == 0:
-                count += 1
-    print(count)
-divisorSubstrings(240, 2)
+    # do i need to sort? 
+    # sort the array
+    # make a variable to track the current lowest diff
+    # loop through all and comparing differences between [0]and[-1]
+    # sliding window for comparison 
+    # use k to get the window
+    # return lowest diff variable
+
+    nums.sort()
+    lowestDelta = 0
+    if len(nums) == 1 :
+        lowestDelta = 0
+    elif len(nums) == k:
+        lowestDelta = nums[-1] - nums[0]
+    else:
+        lowestDelta = nums[k] - nums[0]
+    for i in range(len(nums)-k+1):
+        windowDelta = nums[i:i+k]
+        highest = windowDelta[-1]
+        lowest = windowDelta[0]
+        delta = highest - lowest
+
+        if (lowestDelta > delta):
+            lowestDelta = delta
+    print(lowestDelta)
+minimumDifference([4407,3036], 2)
