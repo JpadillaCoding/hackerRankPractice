83e1e85a9fc4766c47cbfb11e2d3ceea7243e588
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-25 07:36:23 -0600

    561 array partition: solved

--- practice.py
+++ practice.py
@@ -1,69 +1,15 @@
-def findWinner(s):
-    problemString = list(s)
-    global Alice
-    Alice = True
-    global gameComplete
-    gameComplete = False
-
-
-    def playerTurn():
-        global gameComplete
-        global Alice
-        print("Turn: " ,Alice)
-        flipsMade = False
-        playerLetter = ''
-        if Alice == True:
-            playerLetter = 'w'
-        else:
-            playerLetter = 'b'
-        for i in range(1, len(problemString) - 1):
-            if problemString[i] == playerLetter and problemString[i+1] == playerLetter and problemString[i-1] == playerLetter:
-                del problemString[i]
-                Alice = not Alice
-                flipsMade = True
-                break
-        if flipsMade == False:
-            gameComplete = True
-    
-    while gameComplete == False:
-        playerTurn()
-
-
-    print("Final: " ,Alice)
-
-findWinner('wwwbbbbwww')
-
-
-
-""" def gameWinner(colors):
-    problemString = list(colors)  # Convert string to list for easy modification
-    i = 1  # Start from the second character
-    wendyTurn = True
-    def game(player, problemString):
-        while i < len(problemString) - 1:
-            if problemString[i-1] == 'w' and problemString[i] == 'w' and problemString[i+1] == 'w':
-                # Remove 'w' at the current index
-                problemString.pop(i)
-                wendyTurn = not wendyTurn
-                break
-            else:
-                i += 1  # Move to the next character
-
-        result = ''.join(problemString)
-        print(result)
-gameWinner('wwwbbbbwww') """
-
-
-""" def subarraySum(arr):
-    arrLen = len(arr)
-    result = 0
-    for start in range(arrLen):
-        for j in range(start, arrLen):
-            subArraySum = 0
-            for k in range(i, j+ 1):
-                subArraySum += arr[k]
-            result += subArraySum
-    return result
-subarraySum([4,5,6]) """
+def arrayPairSum(arr):
+    # Get the maximum number that can be made with all minumum pairs 
+    # easiest way to do this sort all
+    # check to make sort works here unlike js
+    # return the same of all pairs
+    # can increase time by adding algo to sort 
+    arr.sort()
+    total = 0
+    for i in range(0,len(arr),2):
+        total += arr[i]
+    print(total)
+arrayPairSum([6,2,6,5,1,2])
+# [6,2,6,5,1,2]
     
 
