bddcbc1caa18a906af551620c622ecbc3ff4730c
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-28 09:01:44 -0600

    455 assign cookies: solved optimal

--- practice.py
+++ practice.py
@@ -1,26 +1,20 @@
 def findContentChildren(g, s):
-    # already sorted? 
-    # make a satisfied variable
-    # start with the smallest greed (g)
-        # check if the size (s) is equal or greater to g 
-        # +1 to satisfied 
-        # else return satisfied
-    # how to traverse? 
-    # outer loop looking at g 
-    # inner loop looking at s and updating its position
+    # double pointer system left is g right is s 
+    # 
+    g.sort()
     s.sort()
-    s.sort()
-    satisfied = 0 
-    jStart = 0
-    for i in range(len(g)):
 
-        for j in range(jStart,len(s)):
-            if s[j] >= g[i]:
-                satisfied +=1
-                jStart +=1
-                break
-            else:
-                jStart += 1
-    print(satisfied)
+    left = 0 
+    right = 0 
+    counter = 0
+
+    while left < len(g) and right < len(s):
+        # while loop on left to move by one 
+        if s[right] >= g[left]:
+            counter += 1
+            right +=1
+        left += 1
+    print(counter)
+        
 
 findContentChildren([1,2],[1,2,3])
