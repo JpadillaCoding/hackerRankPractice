c1d6406e9b180eae5e9f9fd30e7eaf83098d2539
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-28 10:43:54 -0600

    680 valid palindrome II: solved optimal

--- practice.py
+++ practice.py
@@ -1,39 +1,15 @@
 import math
 def validPalindrome(s):
-    # needs to read the same forward and back
-    stringLen = len(s)
-    # make descrpency counter
-    descrepency = 0
-    left = 0 
-    right = len(s) -1
-    # if even then add evenly both sides 
-    if stringLen % 2 == 0:
-        #double pointer system 
-        for left in range(stringLen // 2):
+    def verify(s,left, right, deleted):
+        while left < right:
             if s[left] != s[right]:
-                descrepency += 1
-            right -= 1
-            if descrepency > 1:
-                print(False)
-        if descrepency <= 1:
-            print(True)
-        else:
-            print(False)
-    #else:
-    # if odd then half from left and right of middle
-    else: 
-        for left in range(math.floor(stringLen//2)-1):
-            if s[left] != s[right]:
-                descrepency += 1
-            right -=1
-            if descrepency > 1:
-                print(False)
-        if descrepency <= 1:
-            print(True)
-        else:
-            print(False)
-        # if descrepency counter <= 1
-            #return True
-        # else False
-
-validPalindrome('adcdcba')
+                if deleted:
+                    return (False)
+                else:
+                    return verify(s, left + 1, right, True) or verify(s, left, right - 1, True)
+            else:
+                left += 1
+                right -= 1
+        return(True)
+    return verify(s, 0, len(s) - 1, False)
+validPalindrome('abc')
