fcf6e3647aebe64947a3cac477abddd92635e334
Jose Padilla <jose.apadilla99@gmail.com>
2023-12-30 09:56:15 -0600

    1013 partition array 3 equal parts: solved optimal

--- practice.py
+++ practice.py
@@ -1,35 +1,26 @@
 def canThreePartsEqualSum(arr):
-    # conditions that need to be met:
-    # sum must be modulo 3 
-    # only way to divide by 3 is every sum being the same
-    # find the amount the array neesd rto be dvided by (avg)
-    # do while 
-    # move left pointer until the avg number is found
-    # move the right pointer until the avg num is found backwards
-    # check if left middle and right are equal
+    # get the total of the array 
+    # if the total can't be divided into 3 equas parts,
+    # return False since it's impossible to parition
+    # get the amount needed for 1/3 of a part
+    # track the amount of partitions. If 1 is found reset the partition total
+    # if a second parition is found, verify the remaining amount if in fact == to avg
     total = sum(arr)
     if total % 3 != 0:
         print(False)
     avg = total // 3 
-    left = 1
-    leftFlipped = False
-    right = len(arr) - 1
-    rightFlipped = False
-    while left < right:
-        leftsum = sum(arr[:left])
-        rightsum = sum(arr[right:])
-        if leftsum != avg:
-            left += 1
-        else:
-            leftFlipped = True
-        if rightsum != avg:
-            right -= 1
-        else:
-            rightFlipped = True
-        if leftFlipped and rightFlipped and sum(arr[left:right]) == avg:
-            print(True)
-        elif leftFlipped and rightFlipped:
-            print(False)
-            break
+    paritionTotal = 0
+    partitions = 0
+
+    for i in range(len(arr)-1):
+        paritionTotal += arr[i]
+        if paritionTotal == avg:
+            if partitions == 1 and sum(arr[i+1:]) == avg:
+                print(True)
+                break
+            partitions +=1
+            paritionTotal = 0
     print(False)
+
+
 canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1])
