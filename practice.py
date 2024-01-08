63178bebeb45ac21118dcd7c2fa537676a28f94e
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-08 00:52:41 -0600

    120 triangle: unsolved

--- practice.py
+++ practice.py
@@ -1,8 +1,18 @@
-def tribonacci(n: int):
-    if n == 1:
-        return 1
-    elif n <= 0: 
-        return 0 
-    else:
-        return (tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3))
-tribonacci(4)
+def minimumTotal(triangle):
+    # tier below has a window of only i + 1 and i - 1 
+    # find min in that window
+    # make new window
+    # keep adding as we go
+    total = triangle[0][0]
+
+    for i in range(1,len(triangle)):
+        lPointer = max(0,i-1)
+        rPointer = min(len(triangle[i]), i+1)
+        # where to track the min inside the subarray? 
+        minFound=triangle[i][lPointer]
+        for j in range(lPointer, rPointer):
+            minFound = min(minFound, triangle[i][j])
+        total += minFound
+    print(total)
+    
+minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]])
