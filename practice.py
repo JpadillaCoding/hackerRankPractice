a6e2f734355dba28094d451e2d0509f39a3b6e55
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-08 00:23:12 -0600

    1137 tribonacci: solved slow

--- practice.py
+++ practice.py
@@ -1,12 +1,8 @@
-def fib(n: int) -> int:
-    nums = []
-    for i in range(n+1):
-    #add the current and previous to the array 
-        if i == 1: 
-            nums.append(1)
-        elif i == 0:
-            nums.append(0)
-        else:
-            nums.append(nums[i-1] + nums[i-2])
-    print(nums[-1])
-fib(3)
+def tribonacci(n: int):
+    if n == 1:
+        return 1
+    elif n <= 0: 
+        return 0 
+    else:
+        return (tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3))
+tribonacci(4)
