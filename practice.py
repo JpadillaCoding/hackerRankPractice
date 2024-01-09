008c2348fda144b5aa0dbd808d3ef3805c66a926
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-08 23:18:53 -0600

    125 valid palindrome: solved

--- practice.py
+++ practice.py
@@ -1,18 +1,18 @@
-def minimumTotal(triangle):
-    # tier below has a window of only i + 1 and i - 1 
-    # find min in that window
-    # make new window
-    # keep adding as we go
-    total = triangle[0][0]
+import re
+def isPalindrome(s):
+    # lowercase all chars
+    # trim all non letters and nums
 
-    for i in range(1,len(triangle)):
-        lPointer = max(0,i-1)
-        rPointer = min(len(triangle[i]), i+1)
-        # where to track the min inside the subarray? 
-        minFound=triangle[i][lPointer]
-        for j in range(lPointer, rPointer):
-            minFound = min(minFound, triangle[i][j])
-        total += minFound
-    print(total)
-    
-minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]])
+    trimmedString = re.sub('[\W_]+', '', s).lower()
+    left = 0
+    right = len(trimmedString)-1
+
+    while left < right:
+        if trimmedString[left] == trimmedString[right]:
+            left += 1
+            right -= 1
+        else:
+            print(False)
+    print(True)
+
+isPalindrome(" ")
