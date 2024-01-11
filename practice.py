1a2278f05cce3aceb951f253ae25d3e47e62b619
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-10 18:23:37 -0600

    Revert "15 3sum: solved unaccepted"
    This reverts commit 3768684e7801d0e6f11a17b81d65bd5e80b0075a.

--- practice.py
+++ practice.py
@@ -1,16 +1,26 @@
-class MinStack:
-    def __init__(self):
-        self.myStack = []
-        
+def threeSum(nums):
+    # find instances in the array where all three added up = 0 
+    # itirate through each number
+    # for each itieration the remainign tow use two pointer system 
+    # use comparison of adding the remainders together is more than 0, reduce right
+    # if less than 0, increase left
+    # check if the set is already made
+    nums.sort()
+    triplets = []
+    for i in range(len(nums) - 1):
+        if nums[i] == 0: print(triplets)
+        left = i + 1
+        right = len(nums) - 1
+        while left < right:
+            if nums[i] + nums[left] + nums[right] == 0:
+                if [nums[i], nums[left], nums[right]] not in triplets:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                left += 1
+            elif nums[i] + nums[left] + nums[right] < 0:
+                left += 1
+            else: 
+                right -= 1
+    print(triplets)
 
-    def push(self, val: int) -> None:
-        self.myStack.append(val)
-
-    def pop(self) -> None:
-        self.myStack.pop()
-
-    def top(self) -> int:
-        return self.myStack[-1]
-
-    def getMin(self) -> int:
-        return min(self.myStack)
+    # return the triplets as their values in arrays. multiple are possible
+threeSum([0,0,0])
