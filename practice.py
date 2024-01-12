cd350587109fd9d9d724d52ecd9755d155bc102f
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-12 14:38:32 -0600

    21 merge two linked list: solved optimal

--- practice.py
+++ practice.py
@@ -1,25 +1,39 @@
-class MinStack:
-    def __init__(self):
-        self.myStack = []
-        self.minStack = []
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-    def push(self, val: int) -> None:
-        self.myStack.append(val)
-        self.minStack.append( val if not self.minStack else min(val, self.minStack[-1]))
-    def pop(self) -> None:
-        self.myStack.pop()
-        self.minStack.pop()
-    def top(self) -> int:
-        return self.myStack[-1]
-    def getMin(self) -> int:
-        return self.minStack[-1]
-    
-minStack = MinStack()
-minStack.push(-2)
-minStack.push(0)
-minStack.push(-3)
-minStack.push(4)
-minStack.getMin()
-minStack.pop()
-minStack.top()    
-minStack.getMin()
+def mergeTwoLists(list1: [ListNode], list2: [ListNode]):
+# needs to be sorted
+# return the head of the new one
+    # traverse list 
+    # if list1[i] > list2[i]:
+        # move list2[i] next to head of list1[i] 
+    # elif move list1[i] next to head of list2[i]
+    # else just do the if 
+    # consider the fact one list can run out and the other doesn't
+    dummy = ListNode(0)
+    tail = dummy
+    while list1 and list2:
+        if list1.value < list2.value:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+    if list1:
+        tail.next = list1
+    elif list2:
+        tail.next = list2
+    return dummy.next
+
+list1 = ListNode(1)
+list1.next = ListNode(2)
+list1.next.next = ListNode(4)
+
+list2 = ListNode(1)
+list2.next = ListNode(3)
+list2.next.next = ListNode(4)
+
+mergeTwoLists(list1, list2)
