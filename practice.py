9005dd9598e469bd405ef1389896073ddd94a1ca
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-12 10:49:09 -0600

    155 min Stack: solved optimal

--- practice.py
+++ practice.py
@@ -1,16 +1,25 @@
 class MinStack:
     def __init__(self):
         self.myStack = []
-        
+        self.minStack = []
 
     def push(self, val: int) -> None:
         self.myStack.append(val)
-
+        self.minStack.append( val if not self.minStack else min(val, self.minStack[-1]))
     def pop(self) -> None:
         self.myStack.pop()
-
+        self.minStack.pop()
     def top(self) -> int:
         return self.myStack[-1]
-
     def getMin(self) -> int:
-        return min(self.myStack)
+        return self.minStack[-1]
+    
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+minStack.push(4)
+minStack.getMin()
+minStack.pop()
+minStack.top()    
+minStack.getMin()
