a6b086f24b1518bce3a6d041d1f0392888dabd58
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-14 14:22:36 -0600

    977 swuares of sorted array: solved followup slow

--- practice.py
+++ practice.py
@@ -2,9 +2,23 @@ def sortedSquares(nums):
 # first make the ability to square. 
 # need to figure out a way to sort in place instead of after
     # possibility of removing the negative signs from each since tht doesn't matter
-    # could also start as the first positive number and have two pointers, one going forward and then going backwards
-    for i in range(len(nums)):
-        nums[i] = nums[i] * nums[i]
-    nums.sort()
-    print(nums)
+    # maybe have an if statement if it is below 0 put into new list with insert at 0 
+    # make new list to return
+        # in this list start with left pointer at start and right pointer at end.
+            # sort as i go
+    # what if when we find the one that is greater than that's the only one that moves
+    squares = []
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        leftSquare = nums[left] * nums[left]
+        rightSquare = nums[right] * nums[right]
+
+        if leftSquare > rightSquare:
+            squares.insert(0,leftSquare)
+            left += 1 
+        else:
+            squares.insert(0,rightSquare)
+            right -=1
+    print(squares) 
 sortedSquares([-4,-1,0,3,10])
