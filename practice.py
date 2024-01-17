5485cd9c5e49bc7fb1be5ada86a46796bb600095
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-17 00:12:27 -0600

    16 3sum closest: failed

--- practice.py
+++ practice.py
@@ -1,15 +1,23 @@
-def isSubsequence(s,t):
-    # two pointer 
-    # one keeps track of s, if letter is found then move s tracker
-    # at end of t tracker then check if s tracker is on last position
-        if len(s) > len(t):return False
-        if len(s) == 0:return True
-        subsequence=0
-        for i in range(0,len(t)):
-            if subsequence <= len(s) -1:
-                print(s[subsequence])
-                if s[subsequence]==t[i]:
+def threeSumClosest(nums,target):
+# do a for loop to through each iteration 
+    # a while loop to use tow pointers to check everything inbetween
+    # a min variable that keeps track of closest amount
+        # find closest amount with min(all 3 - target, currrent min)
+            # use absolute after the different is found to negate negatives
+    closest = abs((nums[0] + nums[1] + nums[-1]) - target)
+    sumTotal = nums[0] + nums[1] + nums[-1]
 
-                    subsequence+=1
-        return  subsequence == len(s) 
-isSubsequence("abc","ahbgdc")
+    for i in range(len(nums)):
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            if abs((nums[i] + nums[left] + nums[right]) - target) < closest:
+                closest = abs((nums[i] + nums[left] + nums[right]) - target)
+                sumTotal = nums[i] + nums[left] + nums[right]
+            if target < closest:
+                right -= 1
+            else:
+                left += 1
+    print(sumTotal)
+threeSumClosest([1,1,1,0],100)
