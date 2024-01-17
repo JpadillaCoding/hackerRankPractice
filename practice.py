b882719e4824fcb337f0e828e4f27225842072cf
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-16 22:25:33 -0600

    392 is subsequence: solved optimal

--- practice.py
+++ practice.py
@@ -2,13 +2,14 @@ def isSubsequence(s,t):
     # two pointer 
     # one keeps track of s, if letter is found then move s tracker
     # at end of t tracker then check if s tracker is on last position
-        if len(s) == 0:
-            return True
-        anchor = 0
-        for explorer in range(len(t)):
-            if t[explorer] == s[anchor]:
-                anchor += 1
-            if anchor == len(s):
-                return(True)
-        return(False)
-isSubsequence("b","abc")
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence <= len(s) -1:
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s) 
+isSubsequence("abc","ahbgdc")
