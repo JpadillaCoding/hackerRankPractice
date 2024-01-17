ded8288c63b3c78f1f147db73aa59ec19651bb5c
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-16 22:01:54 -0600

    392 is subsequence: solved

--- practice.py
+++ practice.py
@@ -1,15 +1,14 @@
-def moveZeroes(nums):
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    # moving it in place with appending a zero and delete in place 
-        # issue of how I am to itirate?
-        # make while loop- if the operation finds zero-
-        # delete the zero but dont itirate forward
-        # if a num is found then itierate forward
-    anchor = 0
-    for explorer in range(len(nums)):
-        if nums[explorer] != 0:
-            nums[anchor], nums[explorer] = nums[explorer], nums[anchor]
-            anchor += 1
-moveZeroes([0,1,0,3,12])
+def isSubsequence(s,t):
+    # two pointer 
+    # one keeps track of s, if letter is found then move s tracker
+    # at end of t tracker then check if s tracker is on last position
+        if len(s) == 0:
+            return True
+        anchor = 0
+        for explorer in range(len(t)):
+            if t[explorer] == s[anchor]:
+                anchor += 1
+            if anchor == len(s):
+                return(True)
+        return(False)
+isSubsequence("b","abc")
