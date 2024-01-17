d2256228f07df421aa5efc294e053c8c1d4eae4c
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-16 20:10:04 -0600

    283 moes zero: solved

--- practice.py
+++ practice.py
@@ -1,22 +1,20 @@
-def sortedSquares(nums):
-# first make the ability to square. 
-# need to figure out a way to sort in place instead of after
-    # possibility of removing the negative signs from each since tht doesn't matter
-    # maybe have an if statement if it is below 0 put into new list with insert at 0 
-    # make new list to return
-        # in this list start with left pointer at start and right pointer at end.
-            # sort as i go
-    # what if when we find the one that is greater than that's the only one that moves
-    squares = [0] * len(nums)
-    left = 0
-    right = len(nums) - 1
-    while left <= right:
-
-        if abs(nums[left]) > abs(nums[right]):
-            squares[right - left] = nums[left] * nums[left]
-            left += 1 
+def moveZeroes(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    # moving it in place with appending a zero and delete in place 
+        # issue of how I am to itirate?
+        # make while loop- if the operation finds zero-
+        # delete the zero but dont itirate forward
+        # if a num is found then itierate forward
+    i = 0
+    length = len(nums)
+    while i < length - 1:
+        if nums[i] == 0:
+            del nums[i]
+            nums.append(0)
+            length -= 1
         else:
-            squares[right-left] = nums[right] * nums[right]
-            right -=1
-    print(squares) 
-sortedSquares([-4,-1,0,3,10])
+            i += 1
+    print(nums)
+moveZeroes([0,1,0,3,12])
