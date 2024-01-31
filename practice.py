c7c0999939ffaca916ac1de730cb2694ade78414
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-31 16:01:50 -0600

    744 smallest letter greater than target: attempt

--- practice.py
+++ practice.py
@@ -1,24 +1,15 @@
-def sortColors(nums):
-    zero = 0
-    one = 0
-    two = 0
 
-    for i in range(len(nums)):
-        if nums[i] == 0:
-            zero += 1
-        if nums[i] == 1:
-            one += 1
-        if nums[i] == 2:
-            two += 1
-    for i in range(len(nums)):
-        if zero > 0:
-            nums[i] = 0
-            zero -= 1
-        elif one > 0:
-            nums[i] = 1
-            one -= 1
-        else:
-            nums[i] = 2
-            two -= 1
-    print(nums)
-sortColors([2,0,2,1,1,0])
+def nextGreatestLetter(letters, target):
+    for i in range(len(letters)):
+        # if on last position then return letters[0]
+        if letters[i] == target:
+            if i == len(letters)-1:
+                print(letters[0])
+            else:
+                print(letters[i + 1])
+        if ord(letters[i]) > ord(target):
+            print(letters[i])
+    # find the first letter that is after the target else letters[0]
+    print(letters[0])
+    # use binary search to find optimal
+nextGreatestLetter(["x","x","y","z"], 'z')
