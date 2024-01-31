81f596bf54ab61c7c88aa0acca6d90efea0d87fa
Jose Padilla <jose.apadilla99@gmail.com>
2024-01-31 17:35:12 -0600

    744 smallest letter greater than target: solved

--- practice.py
+++ practice.py
@@ -1,15 +1,14 @@
 
 def nextGreatestLetter(letters, target):
-    for i in range(len(letters)):
-        # if on last position then return letters[0]
-        if letters[i] == target:
-            if i == len(letters)-1:
-                print(letters[0])
-            else:
-                print(letters[i + 1])
-        if ord(letters[i]) > ord(target):
-            print(letters[i])
-    # find the first letter that is after the target else letters[0]
+    length = len(letters)
+    pointer = length - 1
+    while pointer >= 0:
+        # check if its less than target and not -1
+        if (target == letters[pointer] or ord(target) > ord(letters[pointer])) and pointer == length-1:
+            print(letters[0])
+        elif target == letters[pointer] or ord(target) > ord(letters[pointer]):
+            print(letters[pointer + 1])
+        # check if less than target 
+        pointer -=1
     print(letters[0])
-    # use binary search to find optimal
-nextGreatestLetter(["x","x","y","z"], 'z')
+nextGreatestLetter(["c","f","j"], 'c')
