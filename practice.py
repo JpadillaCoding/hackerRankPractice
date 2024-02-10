fd77cff35d44f24e9fce73b3538aa6605531e5cd
Jose Padilla <jose.apadilla99@gmail.com>
2024-02-10 13:37:36 -0600

    162 find peak element: attempt

--- practice.py
+++ practice.py
@@ -1,15 +1,19 @@
-def findMin(nums):
+def findPeakElement(nums):
+    # binary search 
+    # find spot where left and right of mid are less
+    # or left same as mid and right less
+    # or right same as mid and left less
 
-        left = 0
-        right = len(nums)-1
-        
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] > nums[right]:
-                left = mid + 1
+    left = 0 
+    right = len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) //2
 
-            else:
-                right = mid
-        print(nums[left])
-
-findMin([4,5,6,7,0,1,2,3])
+        if (nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]) or (nums[mid] == nums[mid - 1] and nums[mid] > nums[mid + 1]) or (nums[mid] > nums[mid - 1] and nums[mid] == nums[mid + 1]):
+            print(mid)
+            break
+        if nums[mid] < nums[mid+1]:
+            left = mid
+        else:
+            right = mid
+findPeakElement([1,2,3,1])
