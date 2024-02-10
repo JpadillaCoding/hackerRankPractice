1b299605cd98187a768662b2979db43bd1460adc
Jose Padilla <jose.apadilla99@gmail.com>
2024-02-10 12:42:45 -0600

    153: find min in rotating array: solved optimal

--- practice.py
+++ practice.py
@@ -1,2 +1,15 @@
 def findMin(nums):
-    print(min(nums))
+
+        left = 0
+        right = len(nums)-1
+        
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+
+            else:
+                right = mid
+        print(nums[left])
+
+findMin([4,5,6,7,0,1,2,3])
