1534fbfbca91d22d8f988a7f316ca55374e32182
Jose Padilla <jose.apadilla99@gmail.com>
2024-02-11 15:36:02 -0600

    33 search in rotatetd array: attempt

--- practice.py
+++ practice.py
@@ -1,24 +1,17 @@
-def findPeakElement(nums):
+def search(nums,target):
     # binary search 
-    # find spot where left and right of mid are less
-    # or left same as mid and right less
-    # or right same as mid and left less
-
     left = 0 
     right = len(nums) - 1
-
+    flipped = False
     while left < right:
         mid = left + (right - left) // 2
 
-        if (nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]) or (nums[mid] == nums[mid - 1] and nums[mid] > nums[mid + 1]) or (nums[mid] > nums[mid - 1] and nums[mid] == nums[mid + 1]):
-            #only first conditional is actually needed
-            # at first I thought I would need to check for same adjacent nums
-            # but question says this is not possible
-            print(mid)
-            break
-        if nums[mid] < nums[mid+1]:
-            left = mid + 1
-        else:
-            right = mid - 1
-    print(left)
-findPeakElement([1,2,3,4])
+        if nums[mid] == target:
+            return(mid)
+        if nums[mid] > nums[left]:
+            left = mid 
+        else: 
+            right = mid
+
+    return(-1)
+search([4,5,6,7,0,1,2],0)
