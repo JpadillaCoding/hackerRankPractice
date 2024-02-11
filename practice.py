72422f5ece6b6cf828d77aa95663e7671c33eda6
Jose Padilla <jose.apadilla99@gmail.com>
2024-02-11 15:37:27 -0600

    33 search in rotatetd array: solved optimal

--- practice.py
+++ practice.py
@@ -2,16 +2,27 @@ def search(nums,target):
     # binary search 
     left = 0 
     right = len(nums) - 1
-    flipped = False
-    while left < right:
+
+    while left <= right:
         mid = left + (right - left) // 2
-
         if nums[mid] == target:
-            return(mid)
-        if nums[mid] > nums[left]:
-            left = mid 
-        else: 
-            right = mid
+            print(mid)
 
-    return(-1)
+        # mid is in the left section, we have to switch to right
+        if nums[left] <= nums[mid]:
+            # check if 
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        else:
+            if target < nums[mid]:
+                right = mid - 1
+            elif target > nums[right]:
+                right = mid - 1
+            else: 
+                left = mid + 1
+    return -1
 search([4,5,6,7,0,1,2],0)
