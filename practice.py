c4e2a295306ab32bc6004da350fe9cd5b9c87ca7
Jose Padilla <jose.apadilla99@gmail.com>
2024-05-24 23:02:00 -0500

    94: solved optimal helped

--- practice.py
+++ practice.py
@@ -1,13 +1,12 @@
 class Solution:
-    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root1 and root2:
-            curr = TreeNode(root1.val + root2.val)
+    def inorderTraversal(self, root):
+        result = []
 
-            curr.left = self.mergeTrees(root1.left, root2.left)
-            curr.right = self.mergeTrees(root1.right, root2.right)
-            return curr
-
-        elif root1 and root2 is None:
-            return root1
-        else:
-            return root2
+        def traverse(root, result):
+            if root is None:
+                return None
+            traverse(root.left, result)
+            result.append(root.val)
+            traverse(root.right, result)
+        traverse(root, result)
+        return result
